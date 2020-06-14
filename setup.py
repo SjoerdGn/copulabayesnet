4b565975d8d2dcae56fc13cb2f57276dacf00425
SjoerdGn <38759766+SjoerdGn@users.noreply.github.com>
2020-06-14 14:42:16 +0200

    didn't change much

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ import io
 
 readme = io.open('README.md', encoding='utf-8').read()
 
-requirements = ['numpy>=1.16.0', 
-                'pycopula', 
-                'scipy', 
+requirements = ['numpy>=1.16.0',
+                'pycopula',
+                'scipy',
                 'statsmodels',
-                'matplotlib', 
+                'matplotlib',
                 'scikit-gof',
                 'hydroeval',
                 #'biokit', biokit does not work right now
                 'easygui'
                 ]
 
-setup(name='copulabayesnet', 
+setup(name='copulabayesnet',
       author='Sjoerd Gnodde',
       author_email='',
       license='MIT',
       url = 'https://github.com/SjoerdGn/copulabayesnet',
       keywords = ['Copulas', 'Bayesian Networks', 'Multivariate normal'],
-      version = '0.1.0', 
+      version = '0.1.0',
       description = 'Test different copulas and use multivariate Gaussian copulas',
       long_description = readme,
       install_requires=requirements,
@@ -28,7 +28,7 @@ setup(name='copulabayesnet',
         'tests',
         'scripts',
         )),
-      package_dir = {'':'copulabayesnet'},
+      package_dir = {'':'src'},
       python_requires='>=3.6',
       classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
