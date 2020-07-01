a955483ebd9c2aa72fc088c8fd363e6a3f8a5497
SjoerdGn <38759766+SjoerdGn@users.noreply.github.com>
2020-07-01 22:53:33 +0200

    still testing readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# Additional
+
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
